b4d9d69d6e181afd3c5f34d172f1978aa4c93b07
Michael Neuhold <michael@neuhold.dev>
2023-01-21 00:36:38 +0100

    added docker file for image build in pipeline

--- ftgo-consumer-service/ftgo-consumer-outbound/src/main/kotlin/ftgo/consumer/jpa/ConsumerJpaRepository.kt
+++ ftgo-consumer-service/ftgo-consumer-outbound/src/main/kotlin/ftgo/consumer/jpa/ConsumerJpaRepository.kt
@@ -2,7 +2,6 @@ package ftgo.consumer.jpa
 
 import ftgo.consumer.model.ConsumerEntity
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
 import java.util.*
 
 interface ConsumerJpaRepository: CrudRepository<ConsumerEntity, UUID> {
